c45532f3f8ed59621c869a4690efa0ab13b6ab12
Makda Girmay <makdagirmay4@gmail.com>
2022-04-09 13:06:26 -0400

    adding a script file for the new page

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var todayDate = function(){
+    var date = ducument.querySelector(".date");
+    date.textContent += SVGAnimateMotionElement().format("MM-DD-YYYY")
+};
+
+var citySearch = function(event){
+    event.preventDefault();
+}
+//search for cities
+
+//add a city history button
+
+//finding the coordinates for the city
+
+//
