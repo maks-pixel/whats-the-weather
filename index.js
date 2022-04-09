00b6ef9b8de31f7fcf4ed0924b3ff24dfb5cdec8
Makda Girmay <makdagirmay4@gmail.com>
2022-04-09 13:37:39 -0400

    capturing city input

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
+var cityInput = document.querySelector('#city');
+var city = '';
+var submitBtn = document.querySelector("#submit");
+var pastPlace = [];
 
 var todayDate = function(){
     var date = ducument.querySelector(".date");
     date.textContent += SVGAnimateMotionElement().format("MM-DD-YYYY")
 };
 
+//search for cities
 var citySearch = function(event){
     event.preventDefault();
+    city = cityInput.value.trim();
+    console.log (city);
+    pastPlace.push(city);
+   console.log(pastPlace); 
 }
-//search for cities
+
 
 //add a city history button
 
 //finding the coordinates for the city
 
 //
+
+submitBtn.addEventListener('click', citySearch)
